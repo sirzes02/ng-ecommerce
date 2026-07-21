3ab4e7fab9cdf907de95751e4b05a9eee7c01165
Santiago Varela <sirzes02@gmail.com>
2026-07-20 23:38:28 -0500

    Project Setup & Layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   template: `
-    <h1>Hello, {{ title() }}</h1>
-
+    <app-header></app-header>
     <router-outlet />
   `,
   styles: [],
 })
-export class App {
-  protected readonly title = signal('ng-ecommerce');
-}
+export class App {}
---
+++ src/app/layout/header/header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+import { HeaderActions } from '../header-actions/header-actions';
+
+@Component({
+  selector: 'app-header',
+  imports: [MatToolbar, HeaderActions],
+  templateUrl: './header.html',
+  styles: ``,
+})
+export class Header {}
